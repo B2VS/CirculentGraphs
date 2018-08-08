b5bb8e091b8d827be2dea2bc4ec17e568ac6bd97
Vedic Sharma <mevs2000@gmail.com>
2018-08-08 16:36:48 +0530

    trying m = 8

--- DominSet/main.cpp
+++ DominSet/main.cpp
@@ -2,21 +2,38 @@
 
 using namespace std;
 
-int n_start = 14, n_end = 150;
-int n, m = 7;
+int n_start = 64, n_end = 150;
+int n, m = 8;
 vector <int> DN(1000);
 int combos = 0;
 
 void findDN()
 {
-    for (n = n_start; n <= n_end; ++n)
+    if (m == 7)
     {
-        DN[n] = 1 + (n - 1) / 5;
-        if (n % 5 == 1 && n > 81) DN[n] += 2;
-        else if (n % 5 >= 2 && n != 18 && n != 22 && n != 27) DN[n] += 1;
-        else if (n == 21 || n == 41 || n == 46 || n == 51 || n == 56 || n == 61 || n == 66 ||
-                 n == 71 || n == 76 || n == 81) DN[n] += 1;
+        for (n = n_start; n <= n_end; ++n)
+        {
+            DN[n] = 1 + (n - 1) / 5;
+            if (n % 5 == 1 && n > 81) DN[n] += 2;
+            else if (n % 5 >= 2 && n != 18 && n != 22 && n != 27) DN[n] += 1;
+            else if (n == 21 || n == 41 || n == 46 || n == 51 || n == 56 || n == 61 || n == 66 ||
+                     n == 71 || n == 76 || n == 81) DN[n] += 1;
+        }
     }
+    if (m == 8)
+    {
+        for (n = n_start; n <= n_end; ++n)
+        {
+            DN[n] = 1 + (n - 1) / 5;
+            if (n % 5 == 4 && n > 79 && n != 109) DN[n] += 3;
+            else if (n == 24 || n == 39 || n == 49 || n == 59 || n == 64 || n == 74 || n == 79 ||
+                     n == 109) DN[n] += 2;
+            else if (n % 5 >= 1 && n % 5 <= 3 && n != 17 && n != 21 && n != 23 && n != 27 &&
+                     n != 46) DN[n] += 1;
+            else if (n == 19 || n == 29 || n == 34 || n == 44 || n == 54 || n == 69) DN[n] += 1;
+        }
+    }
+
 }
 
 int cover(vector <bool> &ds)
@@ -78,18 +95,25 @@ bool predictStart2(vector <bool> &ds, int counter = 1, int pos = 0)
     ds[0] = true;
     if (counter == DN[n] || pos + 4 > n)
         return treeSearch(DN[n] - counter, ds);
-    ds[pos + 5] = true;
-    if (predictStart2(ds, counter + 1, pos + 5)) return true;
-    ds[pos + 5] = false;
-    ds[pos + 4] = true;
-    if (predictStart2(ds, counter + 1, pos + 4)) return true;
-    ds[pos + 4] = false;
+    for (int i = 0; i < 2; ++i)
+    {
+        ds[pos + 5 - i] = true;
+        if (predictStart2(ds, counter + 1, pos + 5 - i)) return true;
+        ds[pos + 5 - i] = false;
+    }
+    //if (depth > 2) return predictStart2(ds, depth - 1);
     return false;
 }
 
 bool predictStart(vector <bool> &ds, int base = 0)
 {
-    if (base == 2 && predictStart2(ds)) return true;
+    //if (base == 2 && predictStart2(ds)) return true;
+    if (base == 3)
+    {
+        for (int i = 0; i < n; ++i) ds[i] = false;
+        ds[0] = true;
+        return treeSearch(DN[n] - 1, ds);
+    }
     int i;
     for (i = 0; i < n - 5 * base; ++i)
         ds[i] = !(i%5);
@@ -97,8 +121,45 @@ bool predictStart(vector <bool> &ds, int base = 0)
     return treeSearch(DN[n] - (n - 1) / 5 + base  - 1, ds);
 }
 
+bool closeSearch(int dn, vector <bool> ds, int pos = 0)
+{
+    while (!ds[++pos])
+        if (pos == n) return treeSearch(dn, ds);
+    if (closeSearch(dn, ds)) return true;
+    ds[pos] = false;
+    ds[pos - 1] = true;
+    if (closeSearch(dn, ds)) return true;
+    ds[pos - 1] = false;
+    ds[pos + 1] = true;
+    if (closeSearch(dn, ds)) return true;
+    ds[pos + 1] = false;
+    ds[pos] = true;
+    return false;
+}
+
 int main()
 {
+    n = n_start;
+    vector <bool> ds(n, false);
+    ds[0] = true;
+    ds[1] = true;
+    ds[2] = true;
+    ds[4] = true;
+    ds[6] = true;
+    ds[15] = true;
+    ds[19] = true;
+    ds[21] = true;
+    ds[25] = true;
+    ds[31] = true;
+    ds[36] = true;
+    ds[42] = true;
+    ds[46] = true;
+    ds[48] = true;
+    cout << closeSearch(1, ds) << endl;
+    for (int i = 0; i < n; ++i)
+        if (ds[i]) cout << i << " ";
+    cout << endl;
+    /*
     findDN();
     for (n = n_start; n <= n_end; ++n)
     {
@@ -110,5 +171,5 @@ int main()
         for (int i = 0; i < n; ++i)
             if (ds[i]) cout << i << " ";
         cout << endl;
-    }
+    } */
 }
